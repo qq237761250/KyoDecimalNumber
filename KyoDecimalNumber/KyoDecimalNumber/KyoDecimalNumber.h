0b990936e5975a6b6cc5bc6facf098ffae5b1d60
Kyo <237761250@qq.com>
2020-05-30 11:46:20 +0800

    framework create

---
+++ KyoDecimalNumber/KyoDecimalNumber/KyoDecimalNumber.h
@@ -0,0 +1,19 @@
+//
+//  KyoDecimalNumber.h
+//  KyoDecimalNumber
+//
+//  Created by Kyo on 2020/5/30.
+//  Copyright © 2020 Kyo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KyoDecimalNumber.
+FOUNDATION_EXPORT double KyoDecimalNumberVersionNumber;
+
+//! Project version string for KyoDecimalNumber.
+FOUNDATION_EXPORT const unsigned char KyoDecimalNumberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KyoDecimalNumber/PublicHeader.h>
+
+
